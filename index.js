c4ee51ab10583dec89f50773e75787f8ea8f4900
Narayan <nimishaware8753@gmail.com>
2025-05-19 15:14:19 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import express from "express";
+
+var app = express();
+
+app.get("/user", function (req, res) {
+  res.send("Fetch GET Data From DataBase");
+});
+
+app.post("/user/post", function (req, res) {
+  res.send("POST Data From DataBase");
+});
+
+app.put("/user/put", function (req, res) {
+  res.send("Fetch Data PUT From DataBase");
+});
+
+app.delete("/user/delete", function (req, res) {
+  res.send("DELETE Data From DataBase");
+});
+
+app.listen(3000);
+// npm install -g nodemon # or using yarn: yarn global add nodemon for auto restart
